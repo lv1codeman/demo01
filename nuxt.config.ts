fea64a624d93afc47225410fe177d7bdfd985bc7
lv1codeman <lv1codeman@gmail.com>
2025-09-16 17:07:59 +0800

    成功發布版本1.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.NODE_ENV === "production" ? "/demo01/" : "/",
+    buildAssetsDir: "/static/",
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
